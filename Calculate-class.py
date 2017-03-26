906770a0b85c0c7005bec946551000dd5f3de68d
Andrew Zhang <agz1117@hotmail.com>
2017-03-26 14:28:31 -0700

    update today

--- Calculate-class.py
+++ Calculate-class.py
@@ -3,19 +3,20 @@ Alex helped to create this class
 
 03/10/2017 Add unit test:
 1. Add
+2. Modified on 03/24/2017
 '''
 
 ## Test a python class
 
 class Calculate(object):
     # May or may not define "zeor":
-    #zero = 90
+    zero = 90
 
     def add(self, x, y):
       return x + y + self.zero
 
 if __name__ == '__main__':
     calc = Calculate()
-    calc.zero = 9
+    calc.zero = 10
     result = calc.add(2, 2)
     print ("Result is: ", result)
